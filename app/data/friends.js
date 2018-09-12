abd4e2f6b66d38310b2f9ece86e94002910c3d9f
Nicolas Cueva <nicolascueva@outlook.com>
2018-09-11 21:20:26 -0500

    Added Modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,8 @@
 
 var friendArray = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Betty",
+        "photo": "https://randomuser.me/api/portraits/women/22.jpg",
         "scores": [
             5,
             1,
@@ -15,7 +15,151 @@ var friendArray = [
             4,
             1
         ]
-    }
+    },
+    {
+        "name": "Jack",
+        "photo": "https://randomuser.me/api/portraits/men/86.jpg",
+        "scores": [
+            3,
+            5,
+            5,
+            2,
+            1,
+            1,
+            2,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Maddie",
+        "photo": "https://randomuser.me/api/portraits/women/28.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            1,
+            3,
+            3,
+            3,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Rocko",
+        "photo": "https://randomuser.me/api/portraits/men/23.jpg",
+        "scores": [
+            1,
+            1,
+            2,
+            4,
+            5,
+            5,
+            5,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Ashley",
+        "photo": "https://randomuser.me/api/portraits/women/66.jpg",
+        "scores": [
+            2,
+            2,
+            5,
+            2,
+            2,
+            5,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Nick",
+        "photo": "https://randomuser.me/api/portraits/men/9.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            3,
+            1,
+            3,
+            5,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Jackie",
+        "photo": "https://randomuser.me/api/portraits/women/21.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            5,
+            5,
+            5,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Kyle",
+        "photo": "https://randomuser.me/api/portraits/men/62.jpg",
+        "scores": [
+            2,
+            2,
+            2,
+            4,
+            4,
+            1,
+            2,
+            4,
+            2,
+            4
+        ]
+    },
+    {
+        "name": "Stacy",
+        "photo": "https://randomuser.me/api/portraits/women/77.jpg",
+        "scores": [
+            4,
+            4,
+            2,
+            2,
+            3,
+            4,
+            4,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Andy",
+        "photo": "https://randomuser.me/api/portraits/men/89.jpg",
+        "scores": [
+            1,
+            2,
+            2,
+            3,
+            4,
+            3,
+            3,
+            2,
+            2,
+            5
+        ]
+    },
 ];
 
 module.exports = friendArray;
